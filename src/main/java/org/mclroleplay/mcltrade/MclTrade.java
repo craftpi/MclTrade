77c8e0015a8e52ff0da0e782c4ed9c6209dfbfbf
BuildTools <unconfigured@null.spigotmc.org>
2023-08-27 00:50:44 +0200

    citizens maven dependency

--- src/main/java/org/mclroleplay/mcltrade/MclTrade.java
+++ src/main/java/org/mclroleplay/mcltrade/MclTrade.java
@@ -1,17 +1,24 @@
 package org.mclroleplay.mcltrade;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 
 public final class MclTrade extends JavaPlugin {
+
+    public static PluginDescriptionFile pdf;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        pdf = this.getDescription();
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + pdf.getName() + " " + pdf.getVersion() + " has been enabled.");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + pdf.getName() + " " + pdf.getVersion() + " has been diabled.");
     }
 }
